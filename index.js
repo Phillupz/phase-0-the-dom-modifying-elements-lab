adb4723106af2e21e7d2d85dd057df02af928504
Phillupz <phillup.zukowski@gmail.com>
2022-03-20 22:17:04 -0400

    commit tree 8dd502f84279830e32526cddc70fdc9277f33377

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.textContent = "Phil is the champion"
+
+newHeader.id = "victory"
+
+document.body.append(newHeader)
+
